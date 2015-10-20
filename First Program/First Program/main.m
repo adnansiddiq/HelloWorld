be585c894f290e4bdf61be842e81eb18865298a2
adnansiddiq <adnan.siddiq@live.com>
2015-10-20 23:55:12 +0500

    - Project setup

---
+++ First Program/First Program/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  First Program
+//
+//  Created by Adnan Siddiq on 10/20/15.
+//  Copyright © 2015 jangoodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
